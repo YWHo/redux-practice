014dacf07beb9f2e5494f50c24295d6362cc8686
YWHo <YWHo@users.noreply.github.com>
2018-02-18 19:32:00 +1300

    enable images

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,9 +1,18 @@
 let nextWordId = 0
+let nextImgId = 1000
 
 export const addWord = (word) => {
   return {
     type: 'ADD_WORD',
     id: nextWordId++,
     word
+  }
+}
+
+export const addImage = (imgLink) => {
+  return {
+    type: 'ADD_IMAGE',
+    id: nextImgId++,
+    imgLink
   }
 }
---
+++ client/components/Image.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Image = (props) => (
+  <img src={props.imgLink} alt="given image"/>
+)
+
+export default Image
